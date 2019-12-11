b73ea68c032cfe01d2452115717dcfc3263689e3
sanket-lathiya <lsanket041@gmail.com>
2019-12-12 00:12:28 +0530

    Connecting to heroku DB

--- src/config/connection.js
+++ src/config/connection.js
@@ -1,7 +1,19 @@
 const Sequelize = require('sequelize');
 
-const connection = new Sequelize('MYLARM', 'root', 'sanketlathiya', {
-  host: 'localhost',
+// const connection = new Sequelize('MYLARM', 'root', 'sanketlathiya', {
+//   host: 'localhost',
+//   port: '3306',
+//   dialect: 'mysql',
+//   pool: {
+//     max: 10,
+//     min: 0,
+//     acquire: 30000,
+//     idle: 10000
+//   }
+// });
+
+const connection = new Sequelize('heroku_cb6110f5b720501', 'bddee429e291a0', '859497f0', {
+  host: 'us-cdbr-iron-east-05.cleardb.net',
   port: '3306',
   dialect: 'mysql',
   pool: {
